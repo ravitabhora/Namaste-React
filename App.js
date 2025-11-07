1f34ed073a8bbf9c665b1edbc2153eee3bdce9c3
Ravita Kamra <ravitabhora@hotmail.com>
2025-11-07 17:50:12 +1100

    react episode 2 and 3, parcel, bable, jsx , react component basics

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const header = [
-  React.createElement("div", { id: "parent1" }, [
-    React.createElement("div", { id: "child1" }, [
-      React.createElement("h1", {}, "child1 : This is React from js(h1)."),
-      React.createElement("h2", {}, "This is React from js."),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "This is React from js."),
-      React.createElement("h2", {}, "This is React from js."),
-    ]),
-  ]),
-  React.createElement("div", { id: "parent2" }, [
-    React.createElement("h1", {}, "This is React from js."),
-    React.createElement("h2", {}, "This is React from js."),
-  ]),
-];
+const HeaderComponent = () => (
+  <div>
+    {<TitleComponent/>}
+    <h1>This is Header component.</h1>
+  </div>
+);
+
+const TitleComponent = () => (
+  <div>
+    <h2>This is Title component.</h2>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(header);
+root.render(<HeaderComponent/>)
